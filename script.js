0f4dafe73c9eca4b6d8fcf18d2a03dd32443a68a
arfwjn <134593538+Arfwjn@users.noreply.github.com>
2025-01-17 22:30:22 +0700

    updateScript

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function calculate() {
 
   // This will replace the result of any mathematical operation with "I love you"
   if (expression.match(/[0-9]+[+\-*/][0-9]+/)) {
-    display.value = "I Love You Billa ❤️";
+    display.value = "I Love You Bibah ❤️";
   } else {
     try {
       display.value = eval(expression);
